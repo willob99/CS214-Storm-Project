595a79624731e19036b57cb4b4e030089199bfe8
Will O'Brien <47201102+willob99@users.noreply.github.com>
2021-12-05 19:12:06 -0800

    Update VariableInstancesTopology.java
    VariableInstancesTopology should extend abstract class AbstractVariableInstancesTopology

--- VariableInstancesTopology.java
+++ VariableInstancesTopology.java
@@ -19,12 +19,12 @@ import java.util.Arrays;
 import java.util.Map;
 import java.util.Random;
 
-import java.util.Random;
+import org.apache.storm.starter.AbstractVariableInstancesTopology;
 import org.apache.storm.starter.spout.TestRandomSentenceSpout;
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
-import org.apache.storm.testing.TestWordSpout;
 import org.apache.storm.topology.ConfigurableTopology;
+import org.apache.storm.testing.TestWordSpout;
 import org.apache.storm.topology.OutputFieldsDeclarer;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.topology.base.BaseRichBolt;
@@ -34,45 +34,27 @@ import org.apache.storm.tuple.Values;
 import org.apache.storm.utils.Utils;
 
 /**
- * This is a basic example of a Storm topology.
+ * This is an example of a Storm topology with variable numbers of instances of spouts and bolts.
  */
-public class VariableInstancesTopology extends ConfigurableTopology {
+public class VariableInstancesTopology extends AbstractVariableInstancesTopology {
     public static final String baseDir = "/home/will/testout";
 
-    public static void main(String[] args) throws Exception {
-        ConfigurableTopology.start(new VariableInstancesTopology(), args);
+    public VariableInstancesTopology(String[] args) throws Exception {
+        super(args);
     }
 
-    protected int run(String[] args) {
+    public static void main(String[] args) throws Exception {
+        ConfigurableTopology.start(new VariableInstancesTopology(args), args);
+    }
+
+    protected int run(String[] args) throws Exception {
         TopologyBuilder builder = new TopologyBuilder();
 
-        int numBoltsAndSpouts = 4;
-        ArrayList<Integer> numInstances = new ArrayList<Integer>();
-
-        if (args != null && args.length > 1) {
-            if (args.length - 1 != numBoltsAndSpouts) {
-                System.out.println("wrong number of arguments, defaulting to all 1s");
-                for (int i = 0; i < numBoltsAndSpouts; i++) {
-                    numInstances.add(1);
-                }
-            }
-            else {
-                for (int i = 1; i < args.length; i++) {
-                    numInstances.add(Integer.parseInt(args[i]));
-                }
-            }
-        }
-        else {
-            // If no instance numbers provided, default all to 1.
-            for (int i = 0; i < numBoltsAndSpouts; i++) {
-                numInstances.add(1);
-            }
-        }
-
+        // For testing purposes, log the number of instances in a file.
         try {
             FileWriter fw = new FileWriter(baseDir.concat("/note.txt"), true);
-            fw.write("creating spouts/bolts with these numbers of instances: ");
-            for (int i : numInstances) {
+            fw.write("Creating spouts/bolts with these numbers of instances: ");
+            for (int i : super.numInstances) {
                 fw.write(String.format("%d ", i));
             }
             fw.write("\n");
@@ -81,20 +63,22 @@ public class VariableInstancesTopology extends ConfigurableTopology {
             System.out.println(e);
         }
 
-        // builder.setSpout("word", new TestWordSpout(), numInstances.get(0));
+        if (super.numBoltsAndSpouts != 4) {
+            throw new Exception("Number of spouts and bolts does not match the topology.");
+        }
+
+        // Construct topology with numbers of instances from superclass.
+        // This spout always has 1 instance for testing purposes.
         builder.setSpout("word", new TestWordSpout(), 1);
-        // builder.setSpout("word", new TestRandomSentenceSpout(), numInstances.get(0));
-        // builder.setSpout("word", new TestRandomSentenceSpout(), 1);
-        builder.setBolt("exclaim1", new ExclamationBolt(), numInstances.get(1)).shuffleGrouping("word");
-        builder.setBolt("exclaim2", new ExclamationBolt(), numInstances.get(2)).shuffleGrouping("exclaim1");
-        builder.setBolt("output", new WriteFileBolt(), numInstances.get(3)).shuffleGrouping("exclaim2");
+        builder.setBolt("exclaim1", new ExclamationBolt(), super.numInstances.get(1)).shuffleGrouping("word");
+        builder.setBolt("exclaim2", new ExclamationBolt(), super.numInstances.get(2)).shuffleGrouping("exclaim1");
+        builder.setBolt("output", new WriteFileBolt(), super.numInstances.get(3)).shuffleGrouping("exclaim2");
 
         conf.setDebug(true);
 
-        // Think about number of workers to use
         conf.setNumWorkers(3);
 
-        return submit(args[0], conf, builder);
+        return submit(super.topoName, conf, builder);
     }
 
     public static class ExclamationBolt extends BaseRichBolt {
@@ -151,7 +135,6 @@ public class VariableInstancesTopology extends ConfigurableTopology {
 
         @Override
         public void declareOutputFields(OutputFieldsDeclarer declarer) {
-            // declarer.declare(new Fields("word"));
         }
 
     }
